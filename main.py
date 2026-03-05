73531404d067269efbfd3001f9e066c563ccae66
sld-devops <sandisfifteen@gmail.com>
2026-03-05 22:55:46 +0200

    update comment

--- main.py
+++ main.py
@@ -1,24 +1,25 @@
 def get_book_text(filepath):
     with open(filepath) as file:
-    # open the file at that path; 's' represents the open file
+    # open the file at given path
         return file.read()
-        # give back the entire contents of the file as one long string
+        # read it and give back the entire contents as one long string
     
 
 def count_words(text):
+    # split contents into a list of words on whitespace(no argument), then count them
     num_words = len(text.split())
-    # split the string into a list of words on whitespace(no argument), then count them
     return num_words
-    # send the count back to the caller
+    
 
 def main():
-    path = "books/frankenstein.txt"
     # store the location of our file in a variable
-    text = get_book_text(path)
+    path = "books/frankenstein.txt"
     # call our function, use the path, and save the result in 'text'
-    get_num_words = count_words(text)
-    # call count_words with the book text and store the returned word count
-    print(f"Found {get_num_words} total words")
+    text = get_book_text(path)
+    # call count_words on the content and store the returned word count
+    num_words = count_words(text)
+    print(f"Found {num_words} total words")
 
+    # print(text) - output the contents to the console
 
 main()
