23bbfcea8a269690f58b9294fd795cb3b538df9f
sld-devops <sandisfifteen@gmail.com>
2026-03-08 20:19:11 +0200

    add function sort by alpha char count from higher

--- main.py
+++ main.py
@@ -4,21 +4,32 @@ def get_book_text(filepath):
         # read it and give back the entire contents as one long string
         return file.read()
         
-from stats import get_num_words, get_char_appearance
+from stats import get_num_words, get_char_appearance, sort_on, get_sorted_count
 
 def main():
     # store the location of our file in a variable
     path = "books/frankenstein.txt"
+
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {path}...")
     # call our function, use the path, and save the result in 'text'
     text = get_book_text(path)
     # call count_words on the content and store the returned word count
     num_words = get_num_words(text)
+    print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
-
     # call get_char_appearance on the content and store the returned char count
     char_counts = get_char_appearance(text)
-    print(char_counts)
-
-    # print(text) - output the contents to the console
+    sorted_chars = get_sorted_count(char_counts)
+    print("--------- Character Count -------")
+    # print each char and its counts
+    for item in sorted_chars:
+        char = item["char"]
+        num = item["num"]
+        # skip non-alphabetical characters
+        if not char.isalpha():
+            continue
+        print(f"{char}: {num}")
+    print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -1,9 +1,4 @@
 def get_num_words(text):
-    num_words = len(text.split())
-    return num_words
-
-
-def get_num_words(text):
     # split contents into a list of words on whitespace(no argument)
     num_words = len(text.split())
     return num_words
@@ -11,8 +6,29 @@ def get_num_words(text):
 def get_char_appearance(text):
     seen_counts = {}
     for char in text:
+        # count upper and lower case chars together
         char = char.lower()
+        # create new key if the char appears for first time
         if char not in seen_counts:
             seen_counts[char] = 0
+        # if it has been already seen, count + 1
         seen_counts[char] += 1
+    # give back whole dict when the content is over (all iterations done)
     return seen_counts
+
+def sort_on(seen_counts):
+    return seen_counts["num"]
+
+def get_sorted_count(seen_counts):
+    # create list to append dict with keys later
+    sorted_chars = []
+    # take each char (they are already keys)
+    for char in seen_counts:
+        # take the count for each char (key)
+        count = seen_counts[char]
+        # build a dict of the key pairs, add each dict to list
+        sorted_chars.append({"char": char, "num": count})
+    # after loop, using helper, rearrange the list by "num" key, but make it reverse (originally it's not)
+    sorted_chars.sort(key=sort_on, reverse=True)
+    # give back sorted list with 2 keys into dict
+    return sorted_chars
